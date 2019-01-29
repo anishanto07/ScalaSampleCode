192fbd877fcdf8fbaccd02c3b51d058cbb8aac55
anishanto07 <anish.anto@capgemini.com>
2019-01-29 20:17:48 +0530

    Added printwriter to write output the file
    updating changes to the file

--- HelloWorldScala.scala
+++ HelloWorldScala.scala
@@ -1,29 +1,46 @@
 import scala.io.Source
+import java.io.File
+import java.io.PrintWriter
+
 
 object HelloWorldScala {
-	def main(args: Array[String]) {
+  def main(args: Array[String]) {
 
-		val filename = "C:/Users/poojar745/Documents/Handson/scala/scalaText.txt"
-				val bufferedSource = Source.fromFile(filename)
-				print("Timestamp")
-					for (a <- 1 to 20){
-							print(" ")
-					}
-		    print("|")
-				println("Description")
-				bufferedSource.getLines.foreach(line => {
+    val filename = "C:/Users/poojar745/Documents/Handson/scala/scalaText.txt"
+    
+    val writer = new PrintWriter(new File("C:/Users/poojar745/Documents/Handson/scala/Write.txt"))
 
-					if(line contains "Error"){
-						println("")
-						print(line.substring(20,48))
-						print(" | ")
-						println(line.substring(48,70))
-						for (a <- 1 to 55)
-							print("-")
-					} 
+    val bufferedSource = Source.fromFile(filename)
+    print("Timestamp")
+    writer.write("Timestamp")
+    for (a <- 1 to 20) {
+      print(" ")
+      writer.write(" ")
+    }
+    print("|")
+    writer.write("|")
+    writer.write("Description")
+    println("Description")
+    bufferedSource.getLines.foreach(line => {
 
-				})
+      if (line contains "Error") {
+        println("")
+        writer.write("\r\n")
+        print(line.substring(20, 48))
+        writer.write("\r\n" + line.substring(20, 48))
+        print(" | ")
+        writer.write("|")
+        println(line.substring(48, 70))
+        writer.write(line.substring(48, 70))
+        writer.write("\r\n")
+        for (a <- 1 to 55) {
+          print("-")
+          writer.write("-")
+        }
+      }
+    })
+    writer.close()
 
-	}
+  }
 }
 
